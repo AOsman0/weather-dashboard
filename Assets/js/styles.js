b4edacba40994b085ab864d6435785be22b5ad90
Abdirahman Osman <abdiosman779@gmail.com>
2022-05-08 07:40:13 +0100

    function for jquery with document ready and to add current date

--- Assets/js/styles.js
+++ Assets/js/styles.js
@@ -1,18 +1,4 @@
-// window.addEventListener("load", onReady);
-
-//get current data time stamp
-const currentDateTimeStamp = moment();
-console.log(currentDateTimeStamp);
-
-//use format method
-// Saturday 7th May 20220 9:33pm
-const formattedTime = currentDateTimeStamp.format("dddd,Do,MMM,YYYY h:mmP");
-console.log(formattedTime);
-
-const onReady = () => {
-  console.log("data is ready");
-  renderDate();
-  renderTimeBlocks();
-};
-
-const renderDate = (currentDateTimeStamp) => {};
+$(document).ready(function () {
+  // Handler for .ready() called.
+  $("#currentDay").text(moment().format("dddd, MMM Do"));
+});
