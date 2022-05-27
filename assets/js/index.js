b07f93df8f7ca5a61eef4077e4c9b3fd6d8c7326
AOsman0 <99136448+AOsman0@users.noreply.github.com>
2022-05-27 19:14:58 +0100

    restricted clicks only to LI to see if we are clicking on a list item

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,6 @@
+//target parent
+const recentSearchesContainer = $("#recent-searches-container");
+
 const readFromLocalStorage = (key, defaultValue) => {
   // get from LS using key name
   const dataFromLS = localStorage.getItem(key);
@@ -12,11 +15,7 @@ const readFromLocalStorage = (key, defaultValue) => {
   }
 };
 
-const onReady = () => {
-  console.log("ready");
-  //target parent
-  const recentSearchesContainer = $("#recent-searches-container");
-
+const renderRecentSearches = () => {
   //get recent searches from LS
   // if recent searches is not available what to return is an empty array
   const recentSearches = readFromLocalStorage("recentSearches", []);
@@ -26,13 +25,31 @@ const onReady = () => {
     // if render recent search results
   } else {
     // else empty show alert
-  }
-  const alert = ` <div class="alert alert-warning" role="alert">
+    // create component
+    const alert = ` <div class="alert alert-warning" role="alert">
   You have no recent searches!
   <a href="#" class="alert-link"></a>
 </div>`;
+    //append to parent
+    recentSearchesContainer.append(alert);
+  }
 };
 
+const handleRecentSearchClick = (event) => {
+  //restrict clicks from only li
+  // prep function to accept event object
+  const target = $(event.target);
+  if (target.is("LI")) {
+    console.log("search");
+  }
+};
+
+const onReady = () => {
+  console.log("ready");
+  renderRecentSearches();
+};
+
+recentSearchesContainer.click(handleRecentSearchClick);
 $(document).ready(onReady);
 
 // we need to render search history on load
